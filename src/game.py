ba8c5ce14c679d4df0a619843986618875f4cec5
byAbaddon <kasiopess@abv.bg>
2022-09-07 01:54:05 +0300

    add some state

--- src/game.py
+++ src/game.py
@@ -14,15 +14,17 @@ pygame.display.set_icon(programIcon)
 # add caption
 pygame.display.set_caption('*** Bianka\'s Adventure ***', 'default_icon')
 
+# ========================================================================== global const
 # clock frames
-clock = pygame.time.Clock()
+CLOCK = pygame.time.Clock()
 
 
 # ========================================================================= global methods
 
+
 # draw background
 def background_image(image):
-    bg_image = pygame.image.load(image)
+    bg_image = pygame.image.load(image).convert()
     block_rect = bg_image.get_rect()
     SCREEN.blit(bg_image, (block_rect.x, block_rect.y))
 
@@ -36,11 +38,20 @@ def text_creator(font_size=26, text='No Text', rgb_color=(255, 255, 255),
     SCREEN.blit(input_text, text_position)
 
 
+# keyboard events for exit
+def exit_game():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
 
 # =========================================================================
 
+
 # Game State
-class GameState:
+class GameState():
+
     def __init__(self):
         self.state = 'intro'
 
@@ -49,27 +60,42 @@ class GameState:
         text_creator(26, 'Copyright - 2022', (0, 160, 255), 80, SCREEN_HEIGHT - 20)
         text_creator(26, 'By Abaddon', (0, 160, 255), SCREEN_WIDTH - 60, SCREEN_HEIGHT - 20)
 
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RETURN]:
+            self.state = 'menu'
+        if keys[pygame.K_SPACE]:
+            self.state = 'start_game'
+
+    def menu(self):
+        background_image('../src/assets/images/backgrounds/bg_controls_2.png')
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self.state = 'intro'
+        if keys[pygame.K_LEFT]:
+            self.state = 'legend'
+        if keys[pygame.K_RIGHT]:
+            self.state = 'scores'
+
     def legend(self):
-        background_image('../src/assets/images/backgrounds/bg_controls_img.png')
+        pass
+        # background_image('../src/assets/images/backgrounds/bg_controls.png')
 
     def state_manager(self):
         if self.state == 'intro':
             self.intro()
+        if self.state == 'menu':
+            self.menu()
         if self.state == 'legend':
             self.legend()
+
 
 #  ========================================================================== create new GameState
 game_state = GameState()
 
 # Starting Game
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-
+    exit_game()
     SCREEN.fill(pygame.Color('black'))
     game_state.state_manager()
     pygame.display.update()
-    clock.tick(60)
-    time_counter = pygame.time.get_ticks()
+    CLOCK.tick(60)
